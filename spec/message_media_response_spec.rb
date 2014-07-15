f9643387d664deea81f24c6fe09339d0e2a4d563
Hartwig Brandl <mail@hartwigbrandl.at>
2014-07-15 10:05:03 +0200

    removing all but one test for invalid responses

--- spec/message_media_response_spec.rb
+++ spec/message_media_response_spec.rb
@@ -5,19 +5,8 @@ require 'xml_responses_helper'
 
 describe Rumeme::MessageMediaResponse do
 
-  it 'tests for a valid response' do
-    response = Rumeme::MessageMediaResponse.new(SUCCEEDING_BLOCK_NUMBERS_RESPONSE)
-    expect(response.valid_response?).to eq true
-  end
-
-  it 'tests for a valid response' do
-    response = Rumeme::MessageMediaResponse.new(INVALID_RESPONSE)
-    expect(response.valid_response?).to eq false
-  end
-
-  it 'tests for a failed success when given an invalid response' do
-    response = Rumeme::MessageMediaResponse.new(INVALID_RESPONSE)
-    expect(response.success?).to eq false
+  it 'tests for an invalid valid response' do
+    expect{Rumeme::MessageMediaResponse.new(INVALID_RESPONSE)}.to raise_error
   end
 
   it 'tests for a failed success, valid response with errors' do
@@ -37,21 +26,9 @@ describe Rumeme::MessageMediaResponse do
 
   it 'ensure we get an empty errors array if there are none' do
     response = Rumeme::MessageMediaResponse.new(SUCCEEDING_BLOCK_NUMBERS_RESPONSE)
-    expect(response.valid_response?).to eq true
     expect(response.errors.count).to eq 0
   end
 
-  it 'ensures we get an empty errors array if the response is invalid' do
-    response = Rumeme::MessageMediaResponse.new(INVALID_RESPONSE)
-    expect(response.success?).to eq false
-    expect(response.errors.count).to eq 0
-  end
-
-  it 'ensures we get an empty results_attributes if the response is invalid' do
-    response = Rumeme::MessageMediaResponse.new(INVALID_RESPONSE)
-    expect(response.success?).to eq false
-    expect(response.result_attributes.count).to eq 0
-  end
 
   it 'ensures we get an empty results_attributes array if there are none' do
     response = Rumeme::MessageMediaResponse.new(NO_ATTRIBUTES_RESPONSE)
